c96568b1d301f69aa351cf22cd88582861fe6e6d
Falco Rodenburg <falcorodenburg@gmail.com>
2022-08-27 09:45:37 +0200

    removed stringfication of req body q-a

--- pages/api/testVerifyProof.js
+++ pages/api/testVerifyProof.js
@@ -8,14 +8,12 @@ export default function handler(req, res) {
     } else if( req.method === 'POST'){
         //Need to read these data but they are giving back undefined
         const { externalNullifier , solidityProof , signal, nullifierHash } = req.body
-        const test = req.body;
-        const result = "test result"
+
         console.log(req.body)
         console.log(solidityProof);
         console.log(externalNullifier);
         console.log(signal);
         console.log(nullifierHash);
-        console.log(JSON.stringify(test));
 
         res.status(201).json(nullifierHash)
     }
