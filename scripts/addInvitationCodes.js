5c01efb6da8fd019d27163de1aa772f8657d98fb
Falco Rodenburg <falcorodenburg@gmail.com>
2022-08-28 20:20:31 +0200

    emptied list

--- scripts/addInvitationCodes.js
+++ scripts/addInvitationCodes.js
@@ -6,7 +6,7 @@ const secret = process.env.FAUNA_SECRET_KEY;
 const query = faunadb.query;
 const client = new faunadb.Client({ secret });
 
-const codesList = ["test-code-7", "test-code-8", "test-code-9", "test-code-10", "test-code-11", "test-code-12", "test-code-13", "test-code-14"];
+const codesList = [];
 
 const promises = codesList.map((code) =>
 
