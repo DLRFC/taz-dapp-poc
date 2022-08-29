8d3d11714062818e2f002e01757b6e272c5368d8
danilowhk <danilowhk@gmail.com>
2022-08-29 11:41:40 -0300

    build fix

--- pages/questions-board-page.js
+++ pages/questions-board-page.js
@@ -1,6 +1,4 @@
-// import React, { useState } from 'react'
-import QuestionsBoard from '../components/QuestionsBoard'
-
+import QuestionsBoard from '../components/questionsBoard'
 export default function QuestionsBoardPage() {
   return (
     <div>
