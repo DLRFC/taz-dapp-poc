2e961e5ee31239965f6082e5953f01aa197f4a5d
Rich Warner <richwarneriii@gmail.com>
2022-08-30 00:09:00 -0600

    Styling of QuestionBoard and AnswerBoard

--- test/test.js
+++ test/test.js
@@ -8,11 +8,11 @@ const { verifyProof } = require('@semaphore-protocol/proof');
 const { packToSolidityProof } = require('@semaphore-protocol/proof');
 const { Subgraph } = require('@semaphore-protocol/subgraph')
 const { solidity } = require("../hardhat.config");
+
 const tazMessageAbi = require("../artifacts/contracts/TazMessage.sol/TazMessage.json").abi;
+const USE_EXISTING_TAZ_MESSAGE_CONTRACT = "0xa6E078cc0AD77d69f7Ee28C0A76956C2f1fF47DD";
 
-console.log("Starting test");
-
-describe("TazMessage", function () {
+describe("TazMessage tests", function () {
     let tazMessageContract;
     let signers;
     let groupId;
@@ -23,8 +23,12 @@ describe("TazMessage", function () {
 
     before(async () => {        
         signers = await ethers.getSigners();
-        // tazMessageContract = await run("deployTazMessage", { logs: true });
-        tazMessageContract = await new ethers.Contract("0xa6E078cc0AD77d69f7Ee28C0A76956C2f1fF47DD", tazMessageAbi, signers[0]);
+        if(USE_EXISTING_TAZ_MESSAGE_CONTRACT) {
+            tazMessageContract = await new ethers.Contract(USE_EXISTING_TAZ_MESSAGE_CONTRACT, tazMessageAbi, signers[0]);
+        }
+        else{
+            tazMessageContract = await run("deployTazMessage", { logs: true });
+        }
     })
 
     it("Should add message", async () => {
@@ -32,33 +36,18 @@ describe("TazMessage", function () {
         const messageContent = "My cat responded to their name today. Do I have a dog now?";
         const messageId = 5;
 
-        const newIdentity = new Identity('secret-message');
-        const newIdentityCommitment = newIdentity.generateCommitment();
-        const group = new Group(16);
-        const subgraph = new Subgraph('goerli');
-        const { members } = await subgraph.getGroup('1080', { members: true });
-        group.addMembers(members);
-        // group.addMember(newIdentityCommitment);
-        const externalNullifier = Math.round(Math.random() * 10000);        
-        const signal = messageContent.slice(0, 31);
-        const fullProof = await generateProof(newIdentity, group, externalNullifier, signal, {
-            zkeyFilePath: "static/semaphore.zkey",
-            wasmFilePath: "static/semaphore.wasm"
-        });
-        const { nullifierHash } = fullProof.publicSignals;
-        const solidityProof = packToSolidityProof(fullProof.proof);
-        const signalBytes32 = ethers.utils.formatBytes32String(signal);
-        const groupId = 1080; // fixed at 1080
-
-        console.log("LOG | groupId", groupId);
-        console.log("LOG | newIdentityCommitment", newIdentityCommitment);
-        console.log("LOG | members", members);
-        console.log("LOG | signalBytes32", signalBytes32);
-        console.log("LOG | nullifierHash", nullifierHash);
-        console.log("LOG | externalNullifier", externalNullifier);
-        console.log("LOG | solidityProof", solidityProof);        
+        proofElements = await run("createProof", { logs: true });       
         
-        const tx = await tazMessageContract.addMessage(messageId, messageContent, groupId, signalBytes32, nullifierHash, externalNullifier, solidityProof, { gasLimit: 1500000 });
+        const tx = await tazMessageContract.addMessage(
+            messageId, 
+            messageContent, 
+            proofElements.groupId, 
+            proofElements.signalBytes32, 
+            proofElements.nullifierHash, 
+            proofElements.externalNullifier, 
+            proofElements.solidityProof, 
+            { gasLimit: 1500000 });
+            
         const receipt = await tx.wait();
 
         // console.log("LOG | Message added. Receipt: ", receipt);
@@ -73,37 +62,20 @@ describe("TazMessage", function () {
         const messageId = 2;
         const parentMessageId = 1;
 
-        const newIdentity = new Identity('secret-message');
-        const newIdentityCommitment = newIdentity.generateCommitment();
-        const group = new Group(16);
-        const subgraph = new Subgraph('goerli');
-        const { members } = await subgraph.getGroup('1080', { members: true });
-        group.addMembers(members);
-        // group.addMember(newIdentityCommitment);
-        const externalNullifier = Math.round(Math.random() * 10000);        
-        const signal = messageContent.slice(0, 31);
-        const fullProof = await generateProof(newIdentity, group, externalNullifier, signal, {
-            zkeyFilePath: "static/semaphore.zkey",
-            wasmFilePath: "static/semaphore.wasm"
-        });
-        const { nullifierHash } = fullProof.publicSignals;
-        const solidityProof = packToSolidityProof(fullProof.proof);
-        const signalBytes32 = ethers.utils.formatBytes32String(signal);
-        const groupId = 1080; // fixed at 1080
-
-        console.log("LOG | groupId", groupId);
-        console.log("LOG | newIdentityCommitment", newIdentityCommitment);
-        console.log("LOG | members", members);
-        console.log("LOG | signalBytes32", signalBytes32);
-        console.log("LOG | nullifierHash", nullifierHash);
-        console.log("LOG | externalNullifier", externalNullifier);
-        console.log("LOG | solidityProof", solidityProof);        
+        proofElements = await run("createProof", { logs: true });           
         
-        const tx = await tazMessageContract.replyToMessage(parentMessageId, messageId, messageContent, groupId, signalBytes32, nullifierHash, externalNullifier, solidityProof, { gasLimit: 1500000 });
+        const tx = await tazMessageContract.replyToMessage(
+            parentMessageId, 
+            messageId, 
+            messageContent, 
+            proofElements.groupId, 
+            proofElements.signalBytes32, 
+            proofElements.nullifierHash, 
+            proofElements.externalNullifier, 
+            proofElements.solidityProof, 
+            { gasLimit: 1500000 });
+            
         const receipt = await tx.wait();
-
-        // console.log("LOG | Message added. Receipt: ", receipt);
-        // console.log("LOG | Event emitted. Event: ", receipt.events[0].eventSignature);
         
         expect(receipt.events[1].event).to.equal("MessageAdded");
     });
@@ -114,33 +86,30 @@ describe("TazMessage", function () {
         const messageId = 2;
         const parentMessageId = 0;
 
-        const newIdentity = new Identity('secret-message');
-        const newIdentityCommitment = newIdentity.generateCommitment();
-        const group = new Group(16);
-        const subgraph = new Subgraph('goerli');
-        const { members } = await subgraph.getGroup('1080', { members: true });
-        group.addMembers(members);
-        // group.addMember(newIdentityCommitment);
-        const externalNullifier = Math.round(Math.random() * 10000);        
-        const signal = messageContent.slice(0, 31);
-        const fullProof = await generateProof(newIdentity, group, externalNullifier, signal, {
-            zkeyFilePath: "static/semaphore.zkey",
-            wasmFilePath: "static/semaphore.wasm"
-        });
-        const { nullifierHash } = fullProof.publicSignals;
-        const solidityProof = packToSolidityProof(fullProof.proof);
-        const signalBytes32 = ethers.utils.formatBytes32String(signal);
-        const groupId = 1080; // fixed at 1080
+        proofElements = await run("createProof", { logs: true });  
 
-        console.log("LOG | groupId", groupId);
-        console.log("LOG | newIdentityCommitment", newIdentityCommitment);
-        console.log("LOG | members", members);
-        console.log("LOG | signalBytes32", signalBytes32);
-        console.log("LOG | nullifierHash", nullifierHash);
-        console.log("LOG | externalNullifier", externalNullifier);
-        console.log("LOG | solidityProof", solidityProof);        
+        const tx = await tazMessageContract.replyToMessage(
+            parentMessageId, 
+            messageId, 
+            messageContent, 
+            proofElements.groupId, 
+            proofElements.signalBytes32, 
+            proofElements.nullifierHash, 
+            proofElements.externalNullifier, 
+            proofElements.solidityProof, 
+            { gasLimit: 1500000 });
         
-        await expect(tazMessageContract.replyToMessage(parentMessageId, messageId, messageContent, groupId, signalBytes32, nullifierHash, externalNullifier, solidityProof, { gasLimit: 1500000 })).to.be.revertedWith("Invalid ID provided for parent message");
+        await expect(tazMessageContract.replyToMessage(
+            parentMessageId, 
+            messageId, 
+            messageContent, 
+            proofElements.groupId, 
+            proofElements.signalBytes32, 
+            proofElements.nullifierHash, 
+            proofElements.externalNullifier, 
+            proofElements.solidityProof, 
+            { gasLimit: 1500000 }
+            )).to.be.revertedWith("Invalid ID provided for parent message");
    
     })
 
