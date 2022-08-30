150f4e13839a8fb061ad038047cb6bce11d424b5
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2022-08-30 14:01:28 -0700

    progress

--- components/drawingComponent.js
+++ components/drawingComponent.js
@@ -9,17 +9,7 @@ const Drawing = (props) => {
   const [color, setColor] = React.useState([])
   const isDrawing = React.useRef(false)
   const stageRef = React.useRef(null)
-  const [uriStorage, setUriStorage] = React.useState([
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ])
+  const [uriStorage, setUriStorage] = React.useState([,,,,,,,,])
 
   // useEffect(() => {
   //   setUriStorage(uriStorage);
@@ -74,16 +64,15 @@ const Drawing = (props) => {
 
   const exportUri = () => {
     const uri = stageRef.current.toDataURL()
-
     setUriStorage((uriStorage) => {
-      uriStorage[props.selectedImage] = [
-        ...uriStorage[props.selectedImage],
-        uri.toString(),
-      ]
-      props.uriStorageCallback(uriStorage)
-      console.log(uriStorage)
-      return uriStorage
-    })
+      let uriStorageTemp = uriStorage;
+      uriStorageTemp[props.selectedImage] = uri.toString();
+      console.log("uriStorageTemp:")
+      console.log(uriStorageTemp)
+      return uriStorageTemp
+    });
+    props.uriStorageCallback(uriStorage)
+    setLines([]);
   }
 
   const uriStorageRender = () => {
@@ -101,8 +90,8 @@ const Drawing = (props) => {
 
   return (
     <div className="border-black bg-transparent touch-none">
-      <h1>SKETCHING AREA</h1>
-      <div className="border-black max-w-[250px] border-2 touch-none">
+      <h1 class="text-4xl">SKETCHING AREA</h1>
+      <div className="border-black max-w-[250px] border-2 touch-none bg-white">
         <Stage
           width={250}
           height={250}
@@ -133,7 +122,7 @@ const Drawing = (props) => {
           </Layer>
         </Stage>
       </div>
-      <select
+      {/* <select
         value={tool}
         onChange={(e) => {
           setTool(e.target.value)
@@ -141,10 +130,10 @@ const Drawing = (props) => {
       >
         <option value="pen">Pen</option>
         <option value="eraser">Eraser</option>
-      </select>
-      <button onClick={newColor}>New Color </button>
-      <button onClick={handleUndo}>Undo</button>
-      <button onClick={exportUri}>Export Uri</button>
+      </select> */}
+      <button class="m-2 p-2 rounded-md bg-slate-500" onClick={newColor}>New Color </button>
+      <button class="m-2 p-2 rounded-md bg-slate-500" onClick={handleUndo}>Undo</button>
+      <button class="m-2 p-2 rounded-md bg-slate-500" onClick={exportUri}>Submit</button>
 
       <div className="uriStorageRendering">{/* uriStorageRender() */}</div>
       {/* <img src={uriStorage[1]} alt=''></img> */}
