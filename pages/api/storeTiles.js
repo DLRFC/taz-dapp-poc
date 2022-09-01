7b3c649c7802ce98fbd0e3d054ae5dcb4b076856
Falco Rodenburg <falcorodenburg@gmail.com>
2022-09-01 21:47:54 +0200

    basic setup artboad tiles api

---
+++ pages/api/storeTiles.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const uriStorage = [];
+
+  if (req.method === "GET") {
+    // return the current state of the unfinished array
+    res.send(200).json({ uriStorage });
+  } else if (req.method === "POST") {
+    // upddate the unfinished array with latest input
+    const { array } = req.body.uriStorage;
+    uriStorage = array;
+  }
+}
