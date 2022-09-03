82f781cb2028c514b11da96b78fa97ae885a061d
Colleen Rose <cjrose59@gmail.com>
2022-09-03 14:20:29 -0700

    add button component

--- components/questionsBoard.js
+++ components/questionsBoard.js
@@ -1,13 +1,14 @@
-import Header from './Header'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import Header from "./Header";
+import Button from "./Button";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SUGBRAPH_TAZ_MESSAGE =
-  'https://api.thegraph.com/subgraphs/name/dlrfc/taz-message-goerli'
+  "https://api.thegraph.com/subgraphs/name/dlrfc/taz-message-goerli";
 
 const QuestionsBoard = (props) => {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([]);
 
   const fetchQuestions = async () => {
     // Construct query for subgraph
@@ -26,25 +27,25 @@ const QuestionsBoard = (props) => {
         }
       }
       `,
-    }
+    };
     // Fetch data
 
-    let data = []
+    let data = [];
     try {
-      const result = await axios.post(SUGBRAPH_TAZ_MESSAGE, postData)
-      data = result.data.data.messageAddeds
+      const result = await axios.post(SUGBRAPH_TAZ_MESSAGE, postData);
+      data = result.data.data.messageAddeds;
     } catch (err) {
-      console.log('Error fetching subgraph data: ', err)
+      console.log("Error fetching subgraph data: ", err);
     }
-    return data
-  }
+    return data;
+  };
 
   useEffect(() => {
     const doAsync = async () => {
-      setQuestions(await fetchQuestions())
-    }
-    doAsync()
-  }, [])
+      setQuestions(await fetchQuestions());
+    };
+    doAsync();
+  }, []);
 
   return (
     <div className="px-6 py-8 font-sans">
@@ -107,37 +108,15 @@ const QuestionsBoard = (props) => {
         <div className="flex items-center justify-between py-4 px-8 bg-brand-beige">
           <p className="text-2xl text-brand-gray">Q&amp;A</p>
           <Link href="/ask-question-page">
-            <button className="flex justify-between bg-white border-[1px] border-brand-gray py-1 px-3 shadow-[-3px_3px_0px_0px_rgba(71,95,111)]">
-              <p className="text-brand-gray tracking-tighter">Ask a question</p>
-              <svg
-                className="ml-3 mt-1 scale-[120%]"
-                width="14"
-                height="15"
-                viewBox="0 0 14 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M0.45752 7.49992C0.45752 3.81802 3.41692 0.833252 7.06753 0.833252C10.7181 0.833252 13.6775 3.81802 13.6775 7.49992C13.6775 11.1818 10.7181 14.1666 7.06753 14.1666C3.41692 14.1666 0.45752 11.1818 0.45752 7.49992ZM7.06753 2.16659C4.14704 2.16659 1.77952 4.5544 1.77952 7.49992C1.77952 10.4455 4.14704 12.8333 7.06753 12.8333C9.98803 12.8333 12.3555 10.4455 12.3555 7.49992C12.3555 4.5544 9.98803 2.16659 7.06753 2.16659Z"
-                  fill="#BD5141"
-                />
-                <path
-                  transform="translate(3, 3)"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.72857 1.16667C4.72857 0.79848 4.43264 0.5 4.06757 0.5C3.7025 0.5 3.40657 0.79848 3.40657 1.16667V3.83333H0.762563C0.397506 3.83333 0.101562 4.1318 0.101562 4.5C0.101562 4.8682 0.397506 5.16667 0.762563 5.16667H3.40657V7.83333C3.40657 8.20153 3.7025 8.5 4.06757 8.5C4.43264 8.5 4.72857 8.20153 4.72857 7.83333V5.16667H7.37257C7.73764 5.16667 8.03357 4.8682 8.03357 4.5C8.03357 4.1318 7.73764 3.83333 7.37257 3.83333H4.72857V1.16667Z"
-                  fill="#BD5141"
-                />
-              </svg>
-            </button>
+            <div>
+              <Button text="Ask a question" />
+            </div>
           </Link>
         </div>
 
         {questions.map((question) => (
           <Link
-            href={'/answers-board-page/' + question.messageId}
+            href={"/answers-board-page/" + question.messageId}
             key={question.id}
           >
             <div className="flex w-full flex-row items-center border-b-[1px] border-brand-gray p-3 cursor-pointer">
@@ -169,7 +148,7 @@ const QuestionsBoard = (props) => {
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuestionsBoard
+export default QuestionsBoard;
