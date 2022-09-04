d00ce55a964a16439d2463a3cfa555791196ac1c
danilowhk <danilowhk@gmail.com>
2022-09-04 13:57:04 -0300

    architecture change

--- components/ValidateInvitation-test/Index.js
+++ components/ValidateInvitation-test/Index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
+import ValidateInvitationComponent from './View'
 
 const ValidateInvitation = () => {
   const [invitation, setInvitation] = useState('')
@@ -14,16 +15,11 @@ const ValidateInvitation = () => {
   }
 
   return (
-    <div>
-      <p className="bg-gray-100 p-2 mt-2">{invitation}</p>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-700 rounded ml-2"
-        onClick={() => validate()}
-      >
-        Validate Invitation
-      </button>
-      {response ? <p className="bg-gray-100 p-2 mt-2">{response}</p> : ''}
-    </div>
+    <ValidateInvitationComponent
+      invitation={invitation}
+      validate={validate}
+      response={response}
+    />
   )
 }
 
