f30cc58f0221973af0f5ec1ddcb4036b3d12dae5
danilowhk <danilowhk@gmail.com>
2022-09-04 18:52:37 -0300

    api updates

--- components/LoadingModal/View.js
+++ components/LoadingModal/View.js
@@ -63,7 +63,7 @@ const LoadingModalComponent = ({
             {loadingMessage}
           </div>
           {loadingProof ? (
-            <div className="flex w-full flex-row items-center border-b-[1px] border-brand-gray p-4 break-words text-brand-gray">
+            <div className="flex w-full flex-row items-center border-b-[1px] border-brand-gray p-4 truncate text-brand-gray">
               {loadingProof}
             </div>
           ) : null}
