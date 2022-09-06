41ab636e35824b74441c237476789da7435fb360
danilowhk <danilowhk@gmail.com>
2022-09-06 17:30:00 -0300

    artBoard fix

--- pages/artBoard-page.js
+++ pages/artBoard-page.js
@@ -5,45 +5,10 @@ const ArtBoard = dynamic(() => import('../components/artBoard'), {
   ssr: false,
 })
 
-export async function getServerSideProps(context) {
-  let tiles, canvasId, selectedTile
-  try {
-    const result = await axios.get('/api/modifyCanvas')
-    console.log('result:')
-    console.log(result)
-
-    tiles = result.data.canvas.tiles
-    canvasId = result.data.canvas.canvasId
-
-    // select random tile
-    const remainingIndices = []
-
-    tiles.map((img, i) => {
-      if (img === '') {
-        remainingIndices.push(i)
-      }
-    })
-
-    selectedTile =
-      remainingIndices[
-        Math.floor(Math.random() * (remainingIndices.length - 1))
-      ] || 0
-  } catch (err) {
-    console.log("Error with axios.get('/api/modifyCanvas')", err)
-  }
-  return {
-    props: {
-      tiles,
-      canvasId,
-      selectedTile,
-    },
-  }
-}
-
-export default function ArtBoardPage({ tiles, canvasId, selectedTile }) {
+export default function ArtBoardPage() {
   return (
     <div>
-      <ArtBoard tiles={tiles} canvasId={canvasId} selectedTile={selectedTile} />
+      <ArtBoard />
     </div>
   )
 }
