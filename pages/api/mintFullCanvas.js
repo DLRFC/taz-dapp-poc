ba4813b6aafe553d7b9fadbe2ec6ac381e16640b
Falco Rodenburg <falcorodenburg@gmail.com>
2022-09-06 15:21:06 +0200

    complete mintFullCanvas endpoint

--- pages/api/mintFullCanvas.js
+++ pages/api/mintFullCanvas.js
@@ -1,15 +1,20 @@
 import { create as ipfsHttpClient } from "ipfs-http-client";
 import { ethers } from "ethers";
-import { fromString } from "uint8arrays/from-string";
 import dotenv from "dotenv";
 import faunadb from "faunadb";
+import { Web3Storage, File } from "web3.storage";
+import { Blob } from "@web-std/blob";
+import { TAZTOKEN_CONTRACT } from "../../config/goerli.json";
+
+import TazToken from '../utils/TazToken.json'
 
 dotenv.config({ path: "../../.env.local" });
-// const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
-// const signer = new ethers.Wallet(process.env.PRIVATE_KEY).connect(provider);
-// const abi = TazNFT.abi;
-// const address = process.env.TAZ_NFT_CONTRACT_ADDRESS;
-// const nftContract = new ethers.Contract(address, abi, signer);
+const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY).connect(provider);
+const signerAddress = signer.getAddress();
+const abi = TazToken.abi;
+const contractAddress = TAZTOKEN_CONTRACT;
+const nftContract = new ethers.Contract(contractAddress, abi, signer);
 
 export default async function handler(req, res) {
   if (res.method === "GET") {
@@ -50,27 +55,71 @@ export default async function handler(req, res) {
       } else {
         console.log("canvas is full");
 
-        // add image to IPFS *** Need to have working IPFS RPC API endpoint
-        // const ipfsClient = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+        // This helper function converts a Basea64 string into a Blob (which is what web3.storage needs to create a file).
+        const b64toBlob = (b64Data, contentType = "", sliceSize = 512) => {
+          // First, it decode the Base64-encoded string into a new string with a character for each byte of the binary data.
+          // Each character's code point (charCode) will be the value of the byte.
+          const byteCharacters = Buffer.from(b64Data, "base64").toString(
+            "binary"
+          ); // atob(b64Data) is showing as deprecated
+          // Then, we can create an array of byte values by applying this using the .charCodeAt method for each character in the string.
+          const byteArrays = [];
+          for (
+            let offset = 0;
+            offset < byteCharacters.length;
+            offset += sliceSize
+          ) {
+            const slice = byteCharacters.slice(offset, offset + sliceSize);
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+              byteNumbers[i] = slice.charCodeAt(i);
+            }
+            // We can convert this array of byte values into a real typed byte array by passing it to the Uint8Array constructor.
+            const byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+          }
+          // The byte array can then be converted to a BLOB by wrapping it in an array and passing it to the Blob constructor.
+          const blob = new Blob(byteArrays, { type: contentType });
+          return blob;
+        };
 
-        // const data = JSON.stringify({
-        //   image: imageUri
-        // });
+        const web3StorageApiToken = process.env.WEB3_STORAGE_API_TOKEN;
 
-        // const added = await ipfsClient.add(data);
-        // console.log(added);
-        // const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+        // Image data
+        const contentType = "image/png";
+        const b64Data = imageUri;
 
-        // Send transaction to minting contract 
+        const blobForServingImage = b64toBlob(b64Data, contentType); // Use for serving an image
 
-        // const tx = await nftContract._safeMint(url, { gasLimit: 15000000 });
-        // console.log(tx);
+        const web3StorageClient = new Web3Storage({
+          token: web3StorageApiToken,
+          endpoint: new URL("https://api.web3.storage"),
+        });
 
-        // const response = await tx.wait(3);
-        // console.log(response);
+        const dataFileArrayForServingImage = [
+          new File([blobForServingImage], "image.png"),
+        ];
 
-        // Reset the canvas in the database
-        client.query(
+        let ipfsUrl;
+
+        await web3StorageClient
+          .put(dataFileArrayForServingImage, { wrapWithDirectory: false })
+          .then((dataCid) => {
+            ipfsUrl = "https://" + dataCid + ".ipfs.dweb.link";
+            console.log("IPFS url created: ", ipfsUrl);
+          });
+
+        // Send transaction to TazToken contract
+
+        try {
+        const tx = await nftContract.safeMint(signerAddress, ipfsUrl, { gasLimit: 15000000 });
+        console.log(tx);
+
+        const response = await tx.wait(3);
+        console.log(response);
+        
+        // Reset canvas in database
+        await client.query(
           query.Update(query.Ref(match.ref), {
             data: {
               tiles: ["", "", "", "", "", "", "", "", ""],
@@ -78,7 +127,12 @@ export default async function handler(req, res) {
           })
         );
 
-        res.status(201).json("Canvas minted");
+        // Send response to frontend
+        res.status(201).json("Canvas NFT minted");
+        } catch (e) {
+          console.log(e);
+          res.status(500).json("Error: ", error);
+        }
       }
     } catch (error) {
       res.status(500).json("Error: ", error);
