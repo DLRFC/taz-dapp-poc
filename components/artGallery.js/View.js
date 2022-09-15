9790f267ce407ebf28e2301e8e0617fc1c2a637b
Rowdy <mroddy5280@gmail.com>
2022-09-15 16:19:37 -0600

    Lint fixes for artGallery, ask-question-page, and validateInvitation

--- components/artGallery.js/View.js
+++ components/artGallery.js/View.js
@@ -1,26 +1,13 @@
-// import React, { useEffect, useState } from 'react'
-// import { images } from './data'
-import Button from '../Button'
-import Modal from './Modal'
 import Link from 'next/link'
 
-export default function ArtGalleryComponent({
-  open,
-  handleClose,
-  activeImage,
-  setActiveImage,
-  handleClick,
-  images,
-}) {
+import Button from '../Button'
+import Modal from './Modal'
+
+export default function ArtGalleryComponent({ open, handleClose, activeImage, setActiveImage, handleClick, images }) {
   return (
     <div>
       {open && (
-        <Modal
-          onClose={handleClose}
-          activeImage={activeImage}
-          setActiveImage={setActiveImage}
-          images={images}
-        />
+        <Modal onClose={handleClose} activeImage={activeImage} setActiveImage={setActiveImage} images={images} />
       )}
       <div className="flex flex-col items-center rounded-md px-3">
         <div className="z-10 mt-3 w-full py-3 px-4">
@@ -28,7 +15,9 @@ export default function ArtGalleryComponent({
           <p className="mb-3 py-5 px-3 text-sm text-[#513E2E]">
             Checkout the Arts done by anonymous collaborators!{' '}
             <Link href="/artBoard-page">
-              <a className="underline">Click Here to start drawing</a>
+              <a href="/" className="underline">
+                Click Here to start drawing
+              </a>
             </Link>
           </p>
         </div>
@@ -36,17 +25,13 @@ export default function ArtGalleryComponent({
 
       <div className="flex flex-row flex-wrap justify-center py-10 rounded-xl">
         {images.map((image) => (
-          <ImageCard
-            key={image.id}
-            url={image.uri}
-            onClick={() => handleClick(image.uri)}
-          />
+          <ImageCard key={image.id} url={image.uri} onClick={() => handleClick(image.uri)} />
         ))}
       </div>
       <div className="flex items-center justify-center mb-20">
         <Link href="/artBoard-page">
           <div>
-            <Button text={`Start CoDrawing`} />
+            <Button text="Start CoDrawing" />
           </div>
         </Link>
       </div>
@@ -54,18 +39,17 @@ export default function ArtGalleryComponent({
   )
 }
 
-const ImageCard = ({ url, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="mr-[10px] mb-[10px] relative border-none hover:bg-red-700 cursor-pointer h-[120px] w-[120px] sm:h-[250px] sm:w-[250px] "
-      style={{
-        backgroundImage: `url(${url})`,
-        backgroundSize: 'cover',
-        borderRadius: '5px',
-      }}
-    >
-      <div className="absolute top-0 left-0 right-0 bottom-0 h-[100%] w-[100%] opacity-0 bg-black "></div>
-    </button>
-  )
-}
+const ImageCard = ({ url, onClick }) => (
+  <button
+    type="button"
+    onClick={onClick}
+    className="mr-[10px] mb-[10px] relative border-none hover:bg-red-700 cursor-pointer h-[120px] w-[120px] sm:h-[250px] sm:w-[250px] "
+    style={{
+      backgroundImage: `url(${url})`,
+      backgroundSize: 'cover',
+      borderRadius: '5px'
+    }}
+  >
+    <div className="absolute top-0 left-0 right-0 bottom-0 h-[100%] w-[100%] opacity-0 bg-black " />
+  </button>
+)
--- pages/ask-question-page.js
+++ pages/ask-question-page.js
@@ -1,7 +1,7 @@
-import AskQuestion from '../components/AskQuestion/'
-
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
+
+import AskQuestion from '../components/AskQuestion'
 
 export default function AskQuestionPage() {
   // const [localIdentity, setLocalIdentity] = useState()
@@ -21,11 +21,12 @@ export default function AskQuestionPage() {
         <AskQuestion />
       ) : (
         <>
-          <h1 className="flex items-center justify-center p-10 text-2xl">
-            First Generate an Identity
-          </h1>
+          <h1 className="flex items-center justify-center p-10 text-2xl">First Generate an Identity</h1>
           <Link href="/">
-            <button className="bg-brand-beige2 w-full p-2 border-2 border-brand-gray2 shadow-[-3px_3px_0px_0px_rgba(71,95,111)] mb-8 px-10">
+            <button
+              type="button"
+              className="bg-brand-beige2 w-full p-2 border-2 border-brand-gray2 shadow-[-3px_3px_0px_0px_rgba(71,95,111)] mb-8 px-10"
+            >
               Go to Home
             </button>
           </Link>
