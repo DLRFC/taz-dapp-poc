b8b7b15bc0a9ae5e4c04ac8cb9d562dbf57c2f82
Colleen Rose <cjrose59@gmail.com>
2022-09-15 14:02:24 -0700

    page backgrounds w/svg components

---
+++ components/svgElements/RedCircle.js
@@ -0,0 +1,14 @@
+const RedCircle = () => (
+  <svg
+    // className="absolute -left-2 top-[169px] -z-1"
+    width="139"
+    height="139"
+    viewBox="0 0 139 139"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="69.5" cy="69.5" r="69.5" fill="#BD5141" />
+  </svg>
+)
+
+export default RedCircle
---
+++ components/svgElements/YellowCircle.js
@@ -0,0 +1,14 @@
+const YellowCircle = () => (
+  <svg
+    // className="relative left-[750px] top-[280px]"
+    width="160"
+    height="160"
+    viewBox="0 0 160 160"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="80" cy="80" r="80" fill="#EFAD5F" />
+  </svg>
+)
+
+export default YellowCircle
