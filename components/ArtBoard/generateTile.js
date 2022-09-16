a305ec7611f2ccde784e25979bdb0cba08467572
danilowhk <danilowhk@gmail.com>
2022-09-15 22:36:59 -0400

    ArtBoard Tiles

--- components/ArtBoard/generateTile.js
+++ components/ArtBoard/generateTile.js
@@ -19,13 +19,17 @@ const GenerateTile = forwardRef(
     <td className="bg-white p-0">
       {selectedTile === i ? (
         <div onClick={startDrawing} className="cursor-pointer w-[112px] h-[112px]">
-          <img
-            className="w-[112px] h-[112px]"
-            id={`${i}`}
-            src={
-              tiles[i] ? tiles[i] : '' // "https://media.istockphoto.com/vectors/cartoon-raven-isolated-on-white-background-vector-id597250060?k=20&m=597250060&s=612x612&w=0&h=yl0rXftvQNqXTKQyRjqumexaKiyW6Bq0OFl1Ko4zaAs="
-            }
-          />
+          {tiles[i] ? (
+            <img
+              className="w-[112px] h-[112px]"
+              id={`${i}`}
+              src={
+                tiles[i] ? tiles[i] : '' // "https://media.istockphoto.com/vectors/cartoon-raven-isolated-on-white-background-vector-id597250060?k=20&m=597250060&s=612x612&w=0&h=yl0rXftvQNqXTKQyRjqumexaKiyW6Bq0OFl1Ko4zaAs="
+              }
+            />
+          ) : (
+            <p className="flex items-center justify-center text-center">Click here to start Drawing on this Tile</p>
+          )}
         </div> /* <DrawingHtml
           stageRef={stageRef}
           borderRef={borderRef}
@@ -39,7 +43,8 @@ const GenerateTile = forwardRef(
           className="w-[112px] h-[112px]"
           id={`${i}`}
           src={
-            tiles[i] ? tiles[i] : '' // "https://media.istockphoto.com/vectors/cartoon-raven-isolated-on-white-background-vector-id597250060?k=20&m=597250060&s=612x612&w=0&h=yl0rXftvQNqXTKQyRjqumexaKiyW6Bq0OFl1Ko4zaAs="
+            tiles[i] ? tiles[i] : ''
+            // "https://media.istockphoto.com/vectors/cartoon-raven-isolated-on-white-background-vector-id597250060?k=20&m=597250060&s=612x612&w=0&h=yl0rXftvQNqXTKQyRjqumexaKiyW6Bq0OFl1Ko4zaAs="
           }
         />
       )}
