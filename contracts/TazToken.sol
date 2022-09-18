9504240dbb65eb16e44c22464b653d2460711eef
Rich Warner <richwarneriii@gmail.com>
2022-09-18 13:08:10 -0600

    Contracts and tests updated to include voting and review of content

--- contracts/TazToken.sol
+++ contracts/TazToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "hardhat/console.sol";
@@ -19,18 +19,44 @@ interface ISemaphore {
     ) external;
 }
 
-contract TazToken is ERC721, ERC721URIStorage, Ownable {
+contract TazToken is ERC721, ERC721URIStorage, AccessControl {
     using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    bytes32 public constant TAZ_ADMIN_ROLE = keccak256("TAZ_ADMIN_ROLE");
+    bytes32 public constant REVIEWER_ROLE = keccak256("REVIEWER_ROLE");
+    bytes32 public constant START_STOPPER_ROLE = keccak256("START_STOPPER_ROLE");
 
     // Stores the address of the Semaphore contract used for verifications
     ISemaphore public semaContract;
 
     event NewToken(uint256 tokenId, string uri);
 
+    event VoteAdded(uint256 tokenId);
+
+    event ViolationAdded(uint256 messageNum, address reviewer);
+
+    Counters.Counter private _tokenIdCounter;
+
+    bool private _mintingActive;
+    bool private _votingActive;
+
+    // Constant used for voting so that each identity gets only one vote
+    uint256 private constant _EXTERNAL_NULLIFIER_FOR_VOTING = 115101;
+
     constructor(ISemaphore semaContractAddr) ERC721("TazToken", "TAZ") {
-         semaContract = semaContractAddr;
+        semaContract = semaContractAddr;
+
+        _setRoleAdmin(TAZ_ADMIN_ROLE, TAZ_ADMIN_ROLE);
+        _setRoleAdmin(REVIEWER_ROLE, TAZ_ADMIN_ROLE);
+        _setRoleAdmin(START_STOPPER_ROLE, TAZ_ADMIN_ROLE);
+
+        _grantRole(TAZ_ADMIN_ROLE, msg.sender);
+
+        _mintingActive = true;
+        _votingActive = false;
+
+        // Skip zero
+        _tokenIdCounter.increment();
     }
 
     // Verifies a proof, and on success mints a non-fungible token
@@ -42,13 +68,17 @@ contract TazToken is ERC721, ERC721URIStorage, Ownable {
         bytes32 signal,
         uint256 nullifierHash,
         uint256 externalNullifier,
-        uint256[8] calldata proof) public onlyOwner returns (uint256){
+        uint256[8] calldata proof) public onlyRole(TAZ_ADMIN_ROLE) returns (uint256){
         uint256 tokenId = _tokenIdCounter.current();
+
+        require(_mintingActive, "Minting not active");
 
         // Verify proof with Sempahore contract
         semaContract.verifyProof(groupId, merkleTreeRoot, signal, nullifierHash, externalNullifier, proof);
 
+        // Increment for next use
         _tokenIdCounter.increment();
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
 
@@ -57,7 +87,68 @@ contract TazToken is ERC721, ERC721URIStorage, Ownable {
         return tokenId;
     }
 
-    // The following functions are overrides required by Solidity.
+    function vote(
+        uint256 tokenId,
+        uint256 groupId,
+        uint256 merkleTreeRoot,
+        bytes32 signal,
+        uint256 nullifierHash,
+        uint256[8] calldata proof) external {
+
+        require(_votingActive, "Voting not active");
+
+        // Verify proof with Sempahore contract
+        semaContract.verifyProof(groupId, merkleTreeRoot, signal, nullifierHash, _EXTERNAL_NULLIFIER_FOR_VOTING, proof);
+
+        emit VoteAdded(tokenId);
+    }
+
+    function addStartStoppers(address[] calldata startStoppers) external onlyRole(TAZ_ADMIN_ROLE) {
+        for(uint256 i = 0; i < startStoppers.length; ++i) {
+            grantRole(START_STOPPER_ROLE, startStoppers[i]);
+        }
+    }
+
+    function removeStartStoppers(address[] calldata startStoppers) external onlyRole(TAZ_ADMIN_ROLE) {
+        for(uint256 i = 0; i < startStoppers.length; ++i) {
+            revokeRole(START_STOPPER_ROLE, startStoppers[i]);
+        }
+    }
+
+    function addReviewers(address[] calldata reviewers) external onlyRole(TAZ_ADMIN_ROLE) {
+        for(uint256 i = 0; i < reviewers.length; ++i) {
+            grantRole(REVIEWER_ROLE, reviewers[i]);
+        }
+    }
+
+    function removeReviewers(address[] calldata reviewers) external onlyRole(TAZ_ADMIN_ROLE) {
+        for(uint256 i = 0; i < reviewers.length; ++i) {
+            revokeRole(REVIEWER_ROLE, reviewers[i]);
+        }
+    }
+
+    function startMinting() external onlyRole(START_STOPPER_ROLE) {
+        _mintingActive = true;
+    }
+
+    function stopMinting() external onlyRole(START_STOPPER_ROLE) {
+        _mintingActive = false;
+    }
+
+    function startVoting() external onlyRole(START_STOPPER_ROLE) {
+        _votingActive = true;
+    }
+
+    function stopVoting() external onlyRole(START_STOPPER_ROLE) {
+        _votingActive = false;
+    }
+
+    function addViolation(uint256 tokenId) external onlyRole(REVIEWER_ROLE) {
+        emit ViolationAdded(tokenId, msg.sender);
+    }
+
+    // The following functions are overrides required by Solidity
+
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
@@ -69,5 +160,14 @@ contract TazToken is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- tasks/createProof.js
+++ tasks/createProof.js
@@ -9,8 +9,9 @@ task('createProof', 'create a proof')
   .addParam('identitySeed', 'String used for identity creation', '', types.string)
   .addParam('groupId', 'Semaphore Group ID', 0, types.int)
   .addParam('signal', 'Signal for proof', 'Select Signal', types.string)
+  .addParam('externalNullifier', 'ExternalNullifier', 0, types.int)
   .addOptionalParam('logs', 'Print the logs', true, types.boolean)
-  .setAction(async ({ identitySeed, groupId, signal, logs }, { ethers }) => {
+  .setAction(async ({ identitySeed, groupId, signal, externalNullifier, logs }, { ethers }) => {
     const proofElements = {
       identity: null,
       identityCommitment: null,
@@ -25,26 +26,29 @@ task('createProof', 'create a proof')
       solidityProof: null
     }
 
+    if (logs) {
+      console.log('--------------------------------------------------------------------')
+      console.log('TASK createProof | initiated')
+    }
+
     proofElements.identity = new Identity(identitySeed)
     proofElements.identityCommitment = proofElements.identity.generateCommitment()
-
-    logs && console.log('TASK | identityCommitment', proofElements.identityCommitment)
 
     // Create new group with just this member
     // ! Verification will fail if the on-chain group has different membership
     // Use this if creating a new group or adding a member and executing subsequent steps without time for it to register on subgraph
-    // proofElements.groupId = groupId.toString()
-    // proofElements.group = new Group(16)
-    // proofElements.group.addMember(proofElements.identityCommitment)
-    // proofElements.members = proofElements.group.members
+    proofElements.groupId = groupId.toString()
+    proofElements.group = new Group(16)
+    proofElements.group.addMember(proofElements.identityCommitment)
+    proofElements.members = proofElements.group.members
 
     // Create group using custom subgraph for members (to eliminate 100-member limit)
     // Use this if group and members are already existing
-    proofElements.group = new Group(16)
-    proofElements.groupId = groupId.toString()
-    const subgraphs = new Subgraphs()
-    proofElements.members = await subgraphs.getGroupIdentities(proofElements.groupId)
-    proofElements.group.addMembers(proofElements.members)
+    // proofElements.group = new Group(16)
+    // proofElements.groupId = groupId.toString()
+    // const subgraphs = new Subgraphs()
+    // proofElements.members = await subgraphs.getGroupIdentities(proofElements.groupId)
+    // proofElements.group.addMembers(proofElements.members)
 
     // Create group using Semaphore subgraph for members
     // proofElements.groupId = groupId.toString()
@@ -55,8 +59,7 @@ task('createProof', 'create a proof')
     // proofElements.members = proofElements.group.members
 
     proofElements.merkleTreeRoot = proofElements.group.root
-    logs && console.log('TASK| merkleTreeRoot', proofElements.merkleTreeRoot)
-    proofElements.externalNullifier = Math.round(Math.random() * 1000000000)
+    proofElements.externalNullifier = externalNullifier
     proofElements.signal = signal
     const fullProof = await generateProof(
       proofElements.identity,
@@ -72,7 +75,12 @@ task('createProof', 'create a proof')
     proofElements.solidityProof = packToSolidityProof(fullProof.proof)
     proofElements.signalBytes32 = ethers.utils.formatBytes32String(proofElements.signal)
 
-    logs && console.log('TASK | proofElements', proofElements)
+    logs && console.log('TASK createProof | proofElements', proofElements)
+
+    if (logs) {
+      console.log('TASK createProof | ended')
+      console.log('--------------------------------------------------------------------')
+    }
 
     return proofElements
   })
