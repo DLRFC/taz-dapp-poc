64df42a056f6994c3da1823f0f44cfec6a6ccad8
Colleen Rose <cjrose59@gmail.com>
2022-09-20 07:28:46 -0700

    remove @material

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from "@material-tailwind/react"
 
 import { IdentityContextProvider } from '../context/IdentityContextProvider'
 import '../styles/globals.css'
 import '../styles/drawingComponent.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider>
-    <IdentityContextProvider>
-      <Component {...pageProps} />
-    </IdentityContextProvider>
-  </ThemeProvider>
+  <IdentityContextProvider>
+    <Component {...pageProps} />
+  </IdentityContextProvider>
   
 )
 
