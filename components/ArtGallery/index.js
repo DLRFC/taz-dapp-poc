7b9c53e3ad792943b45ac620c98edb2e9c082152
Rowdy <mroddy5280@gmail.com>
2022-09-21 23:57:17 -0700

    Renamed ArtGallery Folder 2

---
+++ components/ArtGallery/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+
+import ArtGalleryComponent from './View'
+
+export default function ArtGallery(props) {
+  const [images, setImages] = useState(props.images)
+  const [activeImage, setActiveImage] = useState(null)
+  const [open, setOpen] = useState(false)
+
+  const handleClick = (url) => {
+    setActiveImage(url)
+  }
+
+  const updateFromLocalStorage = () => {
+    const savedCanvas = JSON.parse(window.localStorage.getItem('savedCanva'))
+    const found = images.some((element) => savedCanvas && element.uri === savedCanvas.uri)
+    if (found) {
+      window.localStorage.removeItem('savedQuestion')
+    } else if (savedCanvas) {
+      const updatedCanvas = [savedCanvas].concat(images)
+      setImages(updatedCanvas)
+    }
+  }
+
+  useEffect(() => {
+    if (activeImage) {
+      // open modal
+      setOpen(true)
+    }
+    updateFromLocalStorage()
+  }, [activeImage])
+
+  const handleClose = () => {
+    setOpen(false)
+    setActiveImage(null)
+  }
+
+  return (
+    <ArtGalleryComponent
+      open={open}
+      handleClose={handleClose}
+      activeImage={activeImage}
+      setActiveImage={setActiveImage}
+      handleClick={handleClick}
+      images={images}
+    />
+  )
+}
