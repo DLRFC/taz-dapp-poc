d35dfa70ce157e552bf31a2af881e1a513daf4bb
Rowdy <mroddy5280@gmail.com>
2022-09-28 20:35:29 -0700

    Created new Booth Display components

--- pages/booth-display/index.jsx
+++ pages/booth-display/index.jsx
@@ -2,17 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import { Subgraphs } from '../../hooks/subgraphs'
-import Header from '../../components/Header'
-import TazBoothHeaderLogo from '../../components/TazBooth/TazBoothHeaderLogo'
-import TazBoothHeaderDate from '../../components/TazBooth/TazBoothHeaderDate'
+import BoothContent from '../../components/TazBooth/BoothContent'
 import TazBoothFooter from '../../components/TazBooth/TazBoothFooter'
-import TazBoothCanvasGrid from '../../components/TazBooth/TazBoothCanvasGrid'
 
-const BoothDisplay = ({ images, artBoard }) => (
-  <div className="flex flex-col h-full justify-between bg-brand-black">
-    <TazBoothHeaderLogo />
-    <TazBoothHeaderDate />
-    <TazBoothCanvasGrid images={images} />
+const BoothDisplay = () => (
+  <div className="flex flex-col h-screen bg-brand-black p-10">
+    <BoothContent />
     <TazBoothFooter />
   </div>
 )
@@ -20,14 +15,14 @@ const BoothDisplay = ({ images, artBoard }) => (
 export default BoothDisplay
 
 // eslint-disable-next-line no-unused-vars
-export async function getServerSideProps(context) {
-  const subgraphs = new Subgraphs()
-  const imagesRequest = subgraphs.getMintedTokens()
-  // const artBoardRequest = axios.get('/api/modifyCanvas')
+// export async function getServerSideProps(context) {
+//   const subgraphs = new Subgraphs()
+//   const imagesRequest = subgraphs.getMintedTokens()
+//   // const artBoardRequest = axios.get('/api/modifyCanvas')
 
-  const [images] = await Promise.all([imagesRequest])
+//   const [images] = await Promise.all([imagesRequest])
 
-  return {
-    props: { images }
-  }
-}
+//   return {
+//     props: { images }
+//   }
+// }
